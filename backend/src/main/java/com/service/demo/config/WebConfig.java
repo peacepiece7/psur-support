03aff554273aee9b35aa3ce95040f6d456c5874d
peacepiece7 <peacepiece7@naver.com>
2026-01-23 12:42:09 +0900

    add signup page and edit webconfig

--- backend/src/main/java/com/service/demo/config/WebConfig.java
+++ backend/src/main/java/com/service/demo/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.service.demo.interceptor.AuthorizationInterceptor;
 import com.service.demo.resolver.UserSessionResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -30,5 +31,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userSessionResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
